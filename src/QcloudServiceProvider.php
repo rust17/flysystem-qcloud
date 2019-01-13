50cae15007a32eb774c811ba5205e2f1eacdd0f5
circle <newnash@163.com>
2019-01-13 18:22:14 +0800

    add serviceprovider

---
+++ src/QcloudServiceProvider.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Circle33\Flysystem\Qcloud;
+
+use Illuminate\Support\ServiceProvider;
+
+class QcloudServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the package service.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerRoutes();
+
+        $this->publishConfiguration();
+
+        $this->loadViews();
+
+        $this->publishAssets();
+    }
+
+    /**
+     * Register package bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfiguration();
+    }
+
+    /**
+     * Register package routes.
+     *
+     * @return void
+     */
+    public function registerRoutes()
+    {
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+    }
+
+    /**
+     * Publish package configuration.
+     *
+     * @return void
+     */
+    public function publishConfiguration()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/qcloud.php' => config_path('qcloud.php'),
+        ], 'config');
+    }
+
+    /**
+     * Load and publish package views.
+     *
+     * @return void
+     */
+    public function loadViews()
+    {
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'qcloud');
+
+        $this->publishes([
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/qcloud'),
+        ]);
+    }
+
+    /**
+     * Publish package assets.
+     *
+     * @return void
+     */
+    public function publishAssets()
+    {
+        $this->publishes([
+            __DIR__ . '/../public/assets' => public_path('vendor/qcloud'),
+        ], 'assets');
+    }
+}
