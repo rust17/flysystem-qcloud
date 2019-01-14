e46a38a2fa8415b3e9892583849f3adced72c161
circle <newnash@163.com>
2019-01-14 17:57:49 +0800

    add routes

---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use Circle33\Flysystem\Qcloud\Http\Controllers\QcloudController;
+
+Route::group(['namespace' => 'Circle33\\Flysystem\\Qcloud\\Http\\Controllers'], function ($router) {
+    //
+});
---
+++ src/Http/Controllers/QcloudController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Circle33\Flysystem\Qcloud\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class QcloudController extends Controller
+{
+    //
+}
