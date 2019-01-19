f96a48b1b6a248e1e7f34b95cbea63f87da5707e
circle <newnash@163.com>
2019-01-19 17:11:21 +0800

    nav

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,16 +1,16 @@
-<nav class="w-full flex bg-white border-b fixed pin-t z-100 h-16 items-center">
+<nav class="w-full flex bg-white border-b border-green fixed pin-t z-100 h-16 items-center">
   <div class="w-full flex items-center">
-    <div class="lg:w-1/4">
-      <a href="#" class="block no-underline">Qcloud-Flysystem</a>
+    <div class="lg:w-1/4 pl-6">
+      <a href="#" class="block no-underline text-blue-light">Qcloud-Flysystem</a>
     </div>
     <div class="lg:w-3/4">
       <div class="w-full flex justify items-center">
-        <a href="#" class="block flex hover:text-grey-darker items-center mr-6 no-underline">标签1</a>
-        <a href="#" class="block flex hover:text-grey-darker items-center mr-6 no-underline">标签2</a>
-        <a href="#" class="block flex hover:text-grey-darker items-center mr-6 no-underline">标签3</a>
-        <a href="#" class="block flex hover:text-grey-darker items-center mr-6 no-underline">标签4</a>
-        <a href="#" class="block flex hover:text-grey-darker items-center mr-6 no-underline">标签5</a>
-        <a href="#" class="block flex hover:text-grey-darker items-center mr-6 no-underline">标签6</a>
+        <a href="#" class="block text-indigo-light hover:text-grey-darker items-center mr-6 no-underline">标签1</a>
+        <a href="#" class="block text-indigo-light hover:text-grey-darker items-center mr-6 no-underline">标签2</a>
+        <a href="#" class="block text-indigo-light hover:text-grey-darker items-center mr-6 no-underline">标签3</a>
+        <a href="#" class="block text-indigo-light hover:text-grey-darker items-center mr-6 no-underline">标签4</a>
+        <a href="#" class="block text-indigo-light hover:text-grey-darker items-center mr-6 no-underline">标签5</a>
+        <a href="#" class="block text-indigo-light hover:text-grey-darker items-center mr-6 no-underline">标签6</a>
       </div>
     </div>
   </div>
