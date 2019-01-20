85f4d366b017c3697b01dfa302c54841d4e7d74b
circle <newnash@163.com>
2019-01-20 22:35:53 +0800

    list contents user interface

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,16 +1,13 @@
-<nav class="w-full flex bg-white border-b border-green fixed pin-t z-100 h-16 items-center">
+<nav class="header z-100">
   <div class="w-full flex items-center">
     <div class="lg:w-1/4 pl-6">
-      <a href="#" class="block no-underline text-blue-light">Qcloud-Flysystem</a>
+      <a href="{{ route('qclouds.index') }}" class="block no-underline text-blue-light">Qcloud-Flysystem</a>
     </div>
     <div class="lg:w-3/4">
       <div class="w-full flex justify items-center">
-        <a href="#" class="block text-indigo-light hover:text-grey-darker items-center mr-6 no-underline">标签1</a>
-        <a href="#" class="block text-indigo-light hover:text-grey-darker items-center mr-6 no-underline">标签2</a>
-        <a href="#" class="block text-indigo-light hover:text-grey-darker items-center mr-6 no-underline">标签3</a>
-        <a href="#" class="block text-indigo-light hover:text-grey-darker items-center mr-6 no-underline">标签4</a>
-        <a href="#" class="block text-indigo-light hover:text-grey-darker items-center mr-6 no-underline">标签5</a>
-        <a href="#" class="block text-indigo-light hover:text-grey-darker items-center mr-6 no-underline">标签6</a>
+        <a href="#" class="hover:text-grey-darker">文件列表</a>
+        <a href="#" class="hover:text-grey-darker">占位</a>
+        <a href="#" class="hover:text-grey-darker">占位</a>
       </div>
     </div>
   </div>
