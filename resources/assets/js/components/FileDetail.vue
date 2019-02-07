e53498c426381a7a744fb7eeef75266df3c5821f
circle <newnash@163.com>
2019-02-07 08:19:06 +0800

    update file detail

--- resources/assets/js/components/FileDetail.vue
+++ resources/assets/js/components/FileDetail.vue
@@ -1,13 +1,46 @@
 <template>
   <div class="panel">
-    <div>
-      <form class="w-full max-w-xs">
-        <div class="md:flex md:item-center mb-6">
-          <div class="md:w-1/3">
-            <label class="block text-grey font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-file-name">fileName</label>
-          </div>
+    <form class="w-full max-w-xs">
+      <div class="md:flex md:items-center mb-6">
+        <div class="md:w-1/3">
+          <label class="block text-grey font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-file-name">fileName</label>
         </div>
-      </form>
-    </div>
+        <div class="md:w-2/3">
+          <input class="bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-purple" id="inline-file-name" value="">
+        </div>
+      </div>
+      <div class="md:flex md:items-center mb-6">
+        <div class="md:w-1/3">
+          <label class="block text-grey font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-file-size">fileSize</label>
+        </div>
+        <div class="md:w-2/3">
+          <input class="bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-purple" id="inline-file-size" value="">
+        </div>
+      </div>
+      <div class="md:flex md:items-center mb-6">
+        <div class="md:w-1/3">
+          <label class="block text-grey font-bold md:text-right mb-1 md:mb-0 pr-4" for="last-modify-time">lastModifiedTime</label>
+        </div>
+        <div class="md:w-2/3">
+          <input class="bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-purple" id="last-modify-time" value="">
+        </div>
+      </div>
+      <div class="md:flex md:items-center mb-6">
+        <div class="md:w-1/3">
+          <label class="block text-grey font-bold md:text-right mb-1 md:mb-0 pr-4" for="file-url">fileUrl</label>
+        </div>
+        <div class="md:w-2/3">
+          <input class="bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-purple" id="file-url" value="">
+        </div>
+      </div>
+      <div class="md:flex md:items-center mb-6">
+        <div class="md:w-1/3">
+          <label class="block text-grey font-bold md:text-right mb-1 md:mb-0 pr-4" for="file-headers">fileHeader</label>
+        </div>
+        <div class="md:w-2/3">
+          <input class="bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-purple" id="file-headers" value="">
+        </div>
+      </div>
+    </form>
   </div>
 </template>
