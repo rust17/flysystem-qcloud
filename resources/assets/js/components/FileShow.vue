92da8dcd8c2eac84c99b76d5faa6f5f9a2f67182
circle <newnash@163.com>
2019-02-10 23:02:36 +0800

    update file show

--- resources/assets/js/components/FileShow.vue
+++ resources/assets/js/components/FileShow.vue
@@ -6,7 +6,7 @@
           <label class="block font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-file-name">fileName</label>
         </div>
         <div class="md:w-2/3">
-          <input class="bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-purple" id="inline-file-name" value="">
+          <input class="bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-purple" id="inline-file-name" v-model="filename">
         </div>
       </div>
       <div class="md:flex md:items-center mb-6">
@@ -14,7 +14,7 @@
           <label class="block font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-file-size">fileSize</label>
         </div>
         <div class="md:w-2/3">
-          <input class="bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-purple" id="inline-file-size" value="">
+          <input class="bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-purple" disabled id="inline-file-size" value="">
         </div>
       </div>
       <div class="md:flex md:items-center mb-6">
@@ -22,7 +22,7 @@
           <label class="block font-bold md:text-right mb-1 md:mb-0 pr-4" for="last-modify-time">lastModifiedTime</label>
         </div>
         <div class="md:w-2/3">
-          <input class="bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-purple" id="last-modify-time" value="">
+          <input class="bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-purple" disabled id="last-modify-time" value="">
         </div>
       </div>
       <div class="md:flex md:items-center mb-6">
@@ -30,7 +30,7 @@
           <label class="block font-bold md:text-right mb-1 md:mb-0 pr-4" for="file-url">fileUrl</label>
         </div>
         <div class="md:w-2/3">
-          <input class="bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-purple" id="file-url" value="">
+          <input class="bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-purple" disabled id="file-url" value="">
         </div>
       </div>
       <div class="md:flex md:items-center mb-6">
@@ -38,7 +38,13 @@
           <label class="block font-bold md:text-right mb-1 md:mb-0 pr-4" for="file-headers">fileHeader</label>
         </div>
         <div class="md:w-2/3">
-          <input class="bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-purple" id="file-headers" value="">
+          <input class="bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-purple" disabled id="file-headers" value="">
+        </div>
+      </div>
+      <div class="md:flex md:items-center">
+        <div class="md:w-1/3"></div>
+        <div class="md:w-2/3" :class="{ hidden: isHidden }">
+          <button class="shadow bg-purple hover:bg-purple-light focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">Rename File</button>
         </div>
       </div>
     </form>
@@ -47,6 +53,22 @@
 
 <script>
 export default {
-  name: 'FileShow'
+  name: 'FileShow',
+  data() {
+    return {
+      isHidden: true,
+      filename: 'fileName',
+      originFilename: 'fileName'
+    }
+  },
+  watch: {
+    filename(value) {
+      if (value !== this.originFilename) {
+        this.isHidden = false
+      } else {
+        this.isHidden = true
+      }
+    }
+  }
 }
 </script>
