9e369ef17ca92c280b2c6857e0b035c0f26d067b
circle <newnash@163.com>
2019-02-21 17:31:12 +0800

    better upload modal style

--- resources/assets/js/components/UploadFile.vue
+++ resources/assets/js/components/UploadFile.vue
@@ -8,19 +8,18 @@
 
       <div class="px-6 py-4 text-center">
         <button class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded" type="button" @click="alertUpload()">upload file</button>
-        <div class="filezone mt-5 align-middle">
+        <div class="filezone mt-5">
           <input class="hidden" type="file" id="file" ref="file" multiple @change="handleFiles()">
-          <p>
+          <div class="px-6 py-4 flex" v-if="file.length > 0">
+            <div class="flex-1 text-left"><span>{{ file[0].name }}</span></div>
+            <div class="flex-1 text-center"><span>{{ file[0].size }}</span></div>
+            <div class="flex-1 text-right"><a href="javascript:;" class="text-blue hover:text-red" @click="removeFile()">remove</a></div>
+          </div>
+          <p v-else>
             Or drop your files here
           </p>
         </div>
       </div>
-
-      <!-- <div class="px-6 py-4 inline-flex">
-        <div class="flex-1 text-center">{{ filename }}</div>
-        <div class="flex-1 text-center">{{ filesize }}</div>
-        <div class="flex-1 text-center"><a href="javascript:;" class="text-blue hover:text-red" @click="removeFile()">remove</a></div>
-      </div> -->
 
       <div class="px-6 py-4 text-right">
         <button class="bg-blue text-white font-bold py-2 px-4 rounded" type="button" @click="submitFile()" v-show="file.length > 0">Submit</button>
@@ -41,9 +40,7 @@ export default {
   },
   data() {
     return {
-      file: [],
-      filename: '',
-      filesize: '',
+      file: {},
       route: ''
     }
   },
@@ -59,17 +56,19 @@ export default {
 
       this.file = uploadFile
 
-      this.getFilePreview();
+      console.log(this.file)
+
+      // this.getFilePreview();
     },
     getFilePreview() {
       this.$nextTick(() => {
-        this.filename = this.file.filename
-        this.filesize = this.file.filesize
+        this.filename = this.file.name
+        this.filesize = this.file.size
       })
     },
     removeFile() {
-      this.file = ''
-      this.getFilePreview()
+      this.file = null
+      // this.getFilePreview()
     },
     submitFile() {
       if (this.file) {
@@ -105,15 +104,11 @@ export default {
 .filezone {
   outline: 2px dashed grey;
   outline-offset: -10px;
-  background: #bcdefa;
   color: dimgray;
   padding: 10px 10px;
   min-height: 270px;
   position: relative;
   cursor: pointer;
-}
-.filezone:hover {
-  background: #6cb2eb;
 }
 
 .filezone p {
