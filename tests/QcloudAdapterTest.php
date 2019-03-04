70f93ee7e531bd2651f1b78b1ad57ff83da2f117
circle <newnash@163.com>
2019-03-04 14:26:57 +0800

    add tests

---
+++ tests/QcloudAdapterTest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Circle33\Flysystem\Qcloud\Tests;
+
+use Circle33\Flysystem\Qcloud\QcloudAdapter;
+use PHPUnit\Framework\TestCase;
+
+
+class QcloudAdapterTest extends TestCase
+{
+    public function qcloudProvider()
+    {
+
+    }
+
+    public function testWrite()
+    {
+
+    }
+
+    public function testWriteStream()
+    {
+
+    }
+
+    public function testUpdate()
+    {
+
+    }
+
+    public function testUpdateStream()
+    {
+
+    }
+
+    public function testRename()
+    {
+
+    }
+
+    public function testCopy()
+    {
+
+    }
+
+    public function testDelete()
+    {
+
+    }
+
+    public function testDeleteDir()
+    {
+
+    }
+
+    public function testCreateDir()
+    {
+
+    }
+
+    public function testHas()
+    {
+
+    }
+
+    public function testRead()
+    {
+
+    }
+
+    public function testReadStream()
+    {
+
+    }
+
+    public function testListContents()
+    {
+
+    }
+
+    public function testGetMetaData()
+    {
+
+    }
+
+    public function testGetSize()
+    {
+
+    }
+
+    public function testGetMimeType()
+    {
+
+    }
+
+    public function testGetTimestamp()
+    {
+
+    }
+
+    public function testGetOptions()
+    {
+
+    }
+}
